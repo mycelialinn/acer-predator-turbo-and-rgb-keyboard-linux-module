1fc6f9366365c818d913710629034b125c521b4e
Linn <65576637+mycelialinn@users.noreply.github.com>
2026-07-04 14:48:12 -0500

    Make spacing match others listed

--- src/facer.c
+++ src/facer.c
@@ -662,9 +662,9 @@ static struct quirk_entry quirk_acer_predator_pt515_52 = {
 	.gpu_fans = 2,
 };
 static struct quirk_entry quirk_acer_predator_pt516_51s = {
-       	.turbo = 1,
-       	.cpu_fans = 1,
-       	.gpu_fans = 2,
+    .turbo = 1,
+    .cpu_fans = 1,
+    .gpu_fans = 2,
 };
 static struct quirk_entry quirk_acer_predator_pt516_52s = {
 	.turbo = 1,
@@ -1078,7 +1078,7 @@ static const struct dmi_system_id acer_quirks[] __initconst = {
             DMI_MATCH(DMI_PRODUCT_NAME, "Predator PT516-51s"),
         },
         .driver_data = &quirk_acer_predator_pt516_51s,
-        },
+    },
 	{
 		.callback = dmi_matched,
 		.ident = "Acer Predator PT515-52",
